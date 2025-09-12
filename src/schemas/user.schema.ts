97d72fd56ccf59f1329097727de4da27fa0c1a4f
Vishal Maurya <vishal.mauriya@digimantra.com>
2025-09-12 15:28:14 +0530

    env fixes for admin

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -24,7 +24,7 @@ export class User {
     @Prop({ required: true, enum: UserRole })
     role: UserRole;
 
-    @Prop({ required: true })
+    @Prop({ required: false })
     avatarUrl: string;
 
     @Prop({ required: true })
--- src/seeds/admin.seed.ts
+++ src/seeds/admin.seed.ts
@@ -4,11 +4,13 @@ import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { User, UserDocument } from 'src/schemas/user.schema';
 import { UserRole } from 'src/common/enums/user-role.enum';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AdminSeed implements OnApplicationBootstrap {
     constructor(
         @InjectModel(User.name) private userModel: Model<UserDocument>,
+        private readonly configService: ConfigService,
 
     ) { }
 
@@ -16,12 +18,15 @@ export class AdminSeed implements OnApplicationBootstrap {
         const adminExists = await this.userModel.exists({ role: UserRole.ADMIN });
         if (!adminExists) {
             const saltRounds = 10;
-            const hashedPassword = await bcrypt.hash('Admin@123', saltRounds);
+            const adminEmail = this.configService.get<string>('ADMIN_EMAIL');
+            const adminPassword = this.configService.get<string>('ADMIN_PASS');
+
+            const hashedPassword = await bcrypt.hash(adminPassword, saltRounds);
 
             const adminUser = new this.userModel({
                 firstName: 'Vishal',
                 lastName: 'Maurya',
-                email: 'admin@yopmail.com',
+                email: adminEmail,
                 password: hashedPassword,
                 role: UserRole.ADMIN,
                 avatarUrl: null,

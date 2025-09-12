4c32bf09c558d40d3c4c3902cf9a9e884554e340
Vishal Maurya <vishal.mauriya@digimantra.com>
2025-09-12 17:10:59 +0530

    port chnaged

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 7000;
+  const port = configService.get<number>('PORT') || 3000;
 
   // Swagger configuration
   const config = new DocumentBuilder()
